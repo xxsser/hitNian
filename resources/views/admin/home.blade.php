5961311f8c11770c280b6d4767d72ac3d301b5b5
wenkai <wenkai@soi.so>
2016-01-07 12:02:05 +0800

    核销奖品页面修改

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<meta name="csrf-token" content="{{ csrf_token() }}" />
 <div class="container spark-screen">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
@@ -15,21 +14,6 @@
                                 <input type="tel" class="form-control" id="code" value="" />
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <label class="col-lg-4 control-label">姓名</label>
-                            <div class="col-lg-6">
-                                <input type="text" class="form-control" id="name" value="" />
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-lg-4 control-label">电话</label>
-                            <div class="col-lg-6">
-                                <input type="tel" class="form-control" id="phone" value="" />
-                            </div>
-                        </div>
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="button" id="go-submit" class="btn btn-primary" >核销</button>
@@ -50,56 +34,9 @@
             $('#clean').click(function(){
                 $("input").val(null);
             });
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
             $('#go-submit').click(function(){
-                if(isNaN($('#code').val())){
-                    alert("请正确输入核销码");
-                    $('#code').focus();
-                    $('#code').val(null);
-                    return
-                }
-                if ($("#phone").val() != '' && !phoneCheck($("#phone").val())) {
-                    alert("请输入正确的手机号码!");
-                    $('#phone').focus();
-                    $('#phone').val(null);
-                    return;
-                }
-                $.ajax({
-                    url: "/admin/canche_code",
-                    method: 'POST',
-                    data: {code: $("#code").val(),name: $("#name").val(),phone: $("#phone").val()},
-                    success: function(data) {
-                        switch (data.state){
-                            case 'success' :
-                                var str = '核销成功：('+data.gift+') ';
-                                if(data.moneyCount > 0){
-                                    str += '\n现金劵总金额：'+data.moneyCount+'元';
-                                }
-                                alert(str);
-                                break;
-                            case 'codeErr' :
-                                alert('没有找到该核销码，或者该核销码已经被使用');
-                                break;
-                        }
-
-                    },
-                    error: function() {
-                        alert("网络链接出错!");
-                    }
-                })
+                window.location.href = '/ExpiryId/'+$('#code').val();
             });
         });
-        //手机号码验证
-        function phoneCheck(phoneNum) {
-            var pattern  = /^1[34578]\d{9}$/;
-            if (pattern.test(phoneNum)) {
-                return true;
-            }
-            return false
-        }
     </script>
 @endsection
