0d6a3a8be05ef2a0a91bf027f91fbb96b2c17b04
wenkai <wenkai@soi.so>
2016-01-09 13:37:32 +0800

    bug test

--- app/Http/Controllers/WeAuthController.php
+++ app/Http/Controllers/WeAuthController.php
@@ -20,8 +20,9 @@ class WeAuthController extends Controller
 
     //授权用户
     public function wechatAuth(Requests $requests){
+        dd($requests->url());
         $auth = new Auth($this->appId, $this->secret);
-        $user = $auth->authorize($requests->url()); // 返回用户 Bag
+        $user = $auth->authorize(); // 返回用户 Bag
         return $user;
     }
 
