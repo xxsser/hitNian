2d77cab3535adea053e3cb6fdfb7807c69609b89
wenkai <wenkai@soi.so>
2016-01-09 21:49:24 +0800

    Url修正

--- resources/views/nianShou/exprize.blade.php
+++ resources/views/nianShou/exprize.blade.php
@@ -49,7 +49,7 @@
             }
         });
         $('.exchange').click(function(){
-            $.post('exprize/exchange',{pid:$(this).val()},function(data){
+            $.post('{{ URL::action('PrizeController@getExchange') }}',{pid:$(this).val()},function(data){
                 switch (data.state){
                     case 'success' :
                         alert('恭喜，兑换成功！');
