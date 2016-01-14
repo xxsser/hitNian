ee1d56b05982359f578d815f209e9984aa09524c
wenkai <wenkai@soi.so>
2016-01-14 11:29:03 +0800

    发射速度调整

--- public/Js/fireworks.js
+++ public/Js/fireworks.js
@@ -73,7 +73,7 @@ function Firework( sx, sy, tx, ty ) {
         this.coordinates.push( [ this.x, this.y ] );
     }
     this.angle = Math.atan2( ty - sy, tx - sx );
-    this.speed = 2;
+    this.speed = 0.5;
     this.acceleration = 1.05;
     this.brightness = random( 50, 70 );
     // circle target indicator radius
