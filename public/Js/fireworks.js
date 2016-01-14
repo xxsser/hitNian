20aa931602084d5ee8f3ddd61d7c3e63583e9048
wenkai <wenkai@soi.so>
2016-01-14 11:08:17 +0800

    减缓初始效果

--- public/Js/fireworks.js
+++ public/Js/fireworks.js
@@ -25,7 +25,7 @@ var canvas = document.getElementById( 'canvas' ),
     limiterTotal = 5,
     limiterTick = 0,
 // this will time the auto launches of fireworks, one launch per 80 loop ticks
-    timerTotal = 80,
+    timerTotal = 100,
     timerTick = 0,
     mousedown = false,
 // mouse x coordinate,
