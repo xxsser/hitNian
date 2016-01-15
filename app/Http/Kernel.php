d7676647ee7ce057cc125417f9c8e62ae35893b9
wenkai <wenkai@soi.so>
2016-01-15 09:33:30 +0800

    Remove checkFan Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,6 +48,5 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'checkfan' => \App\Http\Middleware\CheckfanMiddleware::class,
     ];
 }
