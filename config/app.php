36985afaf36661bedaad7ccfc8219a7358b0ad94
wenkai <wenkai@soi.so>
2016-01-15 09:58:01 +0800

    Add Wechat Service Providers

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Wechat Service Providers...
+         * */
+        Overtrue\LaravelWechat\ServiceProvider::class,
+
     ],
 
     /*
