5a500e9cea9e583417abcea5f860bab249d9e093
wenkai <wenkai@soi.so>
2016-01-25 09:37:31 +0800

    Modify domain

--- public/Js/main.js
+++ public/Js/main.js
@@ -172,8 +172,8 @@ function setShare(trend){
     return {
         title: '每天摇一摇，摇出年兽大礼包', // 分享标题
         desc: '动动手腕，攻击年兽有奖品，更有iphone6S等豪礼等着你，年兽大战等你来!', // 分享描述
-        link: 'http://hdwyc.3pdj.com/nian/sharelink/'+$('#fid').val(), // 分享链接
-        imgUrl: 'http://hdwyc.3pdj.com/nian/Images/fx.jpg', // 分享图标
+        link: 'http://hdwyc.ldiqq.com/nian/sharelink/'+$('#fid').val(), // 分享链接
+        imgUrl: 'http://hdwyc.ldiqq.com/nian/Images/fx.jpg', // 分享图标
         type: 'link', // 分享类型,music、video或link，不填默认为link
         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
         success: function () {
