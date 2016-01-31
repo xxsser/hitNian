85489a77e65cc3297c4b3dc1b10816d9d0a773f8
wenkai <wenkai@soi.so>
2016-01-31 14:42:25 +0800

    testing

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,23 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
     public function index(){
         return view('admin.report',[
             'count' => self::getCountUser(),
+            'yesterday_count' => self::getCountUser(['form'=>Carbon::yesterday(),'to'=>Carbon::today()]),
+            'yesterday_attack_coin' => self::getAttackCoinNum(),
+            'yesterday_share_coin' => self::getShareCoinNum(),
+            'yesterday_join_num' => self::getAttackFanCount(),
+            'yesterday_share_num' => self::getShareNum(),
+            'yesterday_sharelink_user' => self::getShareLinkUser(),
+            'yesterday_exchang_prize' => self::getExchangeInfo(),
         ]);
     }
 
     private function getCountUser($range=''){
         if(is_array($range)){
-            return \App\Fan::whereBetween('created_at',[$range['from'],$range['to']])->count();
+            return \App\Fan::whereBetween('created_at',[$range['from'],$range['to']])->count('id');
         }
-        return \App\Fan::count();
+        return \App\Fan::count('id');
     }
+
+    private function getAttackCoinNum(){
+        return \App\Attack::whereBrtween('created_at',[Carbon::yesterday(),Carbon::today()])->sum('coin');
+    }
+
+    private function getShareNum(){
+        return \App\Share::whereBrtween('created_at',[Carbon::yesterday(),Carbon::today()])->count('id');
+    }
+
+    private function getShareCoinNum(){
+        return self::getShareNum() * config('customs.shareCoin');
+    }
+
+    private function getAttackFanCount(){
+        return \App\Attack::whereBrtween('created_at',[Carbon::yesterday(),Carbon::today()])->distinct('fan_id')->count('id');
+    }
+
+    private function getShareLinkUser(){
+        return DB::table('recommends')
+            ->whereBrtween('created_at',[Carbon::yesterday(),Carbon::today()])
+            ->count('id');
+    }
+
+    private function getExchangeInfo(){
+        $prizes = \App\Exchange::with(['prize'=>function($query){
+            $query->select('id','name','rank','type');
+        }])->select('id','prize_id','created_at')
+            ->whereBrtween('created_at',[Carbon::yesterday(),Carbon::today()])->unget()->get();
+    }
+
 }
--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -1,1 +1,13 @@
-{{ $count }}
+用户总数：{{ $count }} <br>
+昨日新增用户数: {{ $yesterday_count }} <br>
+昨日攻击掉落总金币： {{ $yesterday_attack_coin }} <br>
+昨日分享送出总金币： {{ $yesterday_share_coin }} <br>
+昨日发放总金币： {{ $yesterday_attack_coin+$yesterday_share_coin }} <br>
+昨日参与活动人数： {{ $yesterday_join_num }} <br>
+昨日分享次数：{{ $yesterday_share_num }} <br>
+通过分享链接首次进入游戏用户数：{{ $yesterday_sharelink_user }} <br>
+
+昨日兑走奖品： <br>
+@foreach($yesterday_exchang_prize as $value)
+    {{ var_dump($value) }}
+@endforeach
